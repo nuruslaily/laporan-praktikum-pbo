75d08c5c19e210dfa0adda228d51c00f8467313c
nuruslaily <nuruslaily88@gmail.com>
2019-09-11 15:51:51 +0700

    test

---
+++ src/3_Enkapsulasi/MotorDemo1841720049nurus.java
@@ -0,0 +1,28 @@
+package motorencapsulation1841720049nurus;
+
+/**
+ *
+ * @author Nurus Laily
+ */
+public class MotorDemo1841720049nurus {
+    public static void main(String[] args){
+        Motor1841720049nurus motor= new Motor1841720049nurus();
+        motor.printStatusNurus();
+        motor.tambahKecepatanNurus();
+        
+        motor.nyalakanMesinNurus();
+        motor.printStatusNurus();
+        
+        motor.tambahKecepatanNurus();
+        motor.printStatusNurus();
+        
+        motor.tambahKecepatanNurus();
+        motor.printStatusNurus();
+        
+        motor.tambahKecepatanNurus();
+        motor.printStatusNurus();
+        
+        motor.matikanMesinNurus();
+        motor.printStatusNurus();
+    }
+}
